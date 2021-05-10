e7cee7fd0a81c4522238f2b7f493dace6cad238f
Noah Teshima <noah.teshima@gmail.com>
2021-05-09 20:29:01 -0700

    Format fix for StandardJS style guide

--- source/javascript/tests/example/example.js
+++ source/javascript/tests/example/example.js
@@ -1,48 +1,46 @@
 class Foo {
-    constructor(str = '', num = 0, arr = []) {
-        this.str = str;
-        this.num = num;
-        this.arr = arr;
+  constructor (str = '', num = 0, arr = []) {
+    this.str = str
+    this.num = num
+    this.arr = arr
+  }
+
+  get string () {
+    return this.str
+  }
+
+  set string (str) {
+    if (typeof (str) !== 'string') {
+      throw new TypeError()
+    }
+    this.str = str
+  }
+
+  get number () {
+    return this.num
+  }
+
+  set number (num) {
+    if (typeof (num) !== 'number') {
+      throw new TypeError()
     }
 
-    get string() {
-        return this.str;
+    this.number = num
+  }
+
+  get array () {
+    return this.arr
+  }
+
+  set array (arr) {
+    if (typeof (arr) !== 'object') {
+      throw new TypeError()
     }
 
-    set string(str) {
-        if (typeof(str) !== 'string') {
-            throw new TypeError();
-        }
-        this.str = str;
-    }
-
-    get number() {
-        return this.num;
-    }
-
-    set number(num) {
-        if (typeof(num) !== 'number') {
-            throw new TypeError();
-        }
-
-        this.number = num;
-    }
-
-    get array() {
-        return this.arr;
-    }
-
-    set array(arr) {
-        if (typeof(arr) !== 'object') {
-            throw new TypeError();
-        }
-
-        this.arrray = arr;
-    }
+    this.arrray = arr
+  }
 }
 
-
-
 module.exports = {
-    Foo: Foo
+  Foo: Foo
 }
--- source/javascript/tests/example/example.test.js
+++ source/javascript/tests/example/example.test.js
@@ -3,71 +3,68 @@
  * Chai for assertions/behavior driven development, and Sinon for
  * mocks/spies/stubs (needed to test AJAX/API calls i.e. components using
  * Fetch API).
- * 
+ *
  * We've outlined some skeleton code containing example hooks and tests.
  */
 
-let Foo = require('./example.js').Foo;
-let sinon = require('Sinon');
-let should = require('chai').should();
+const Foo = require('./example.js').Foo
 
 /**
  * Use describe(..) to create new test suites
  */
 describe('Example test suite', () => {
-    let foo;
+  let foo
 
-    /**
+  /**
      * before(..) runs once before all tests
      */
-    before(() => {
-        console.info("Running example tests");
-    });
+  before(() => {
+    console.info('Running example tests')
+  })
 
-    /**
+  /**
      * before(..) runs once before all tests
      */
-    after(() => {
-        console.info("Finished running example tests");
-    });
+  after(() => {
+    console.info('Finished running example tests')
+  })
 
-    /**
+  /**
      * beforeEach(..) runs once before each unit test
      */
-    beforeEach(() => {
-        foo = new Foo();
-    });
+  beforeEach(() => {
+    foo = new Foo()
+  })
 
-    /**
+  /**
      * afterEach(..) runs once after each unit test
      */
-    afterEach(() => {
-        foo = null;
-    });
+  afterEach(() => {
+    foo = null
+  })
 
-    /**
+  /**
      * describe(..) should also be used for new test cases
      */
-    describe('Example unit test', () => {
-        /**
+  describe('Example unit test', () => {
+    /**
          * it(..) should be a single case for unit tests.
          */
-        it('should run case one', () => {
-            foo.should.be.a('object');
-        });
-
-        /**
-         * This is an example of another case for unit tests.
-         */
-        it('should run case two', () => {
-            foo.should.have.property('arr').with.lengthOf(0);
-        });
-    });
+    it('should run case one', () => {
+      foo.should.be.a('object')
+    })
 
     /**
+         * This is an example of another case for unit tests.
+         */
+    it('should run case two', () => {
+      foo.should.have.property('arr').with.lengthOf(0)
+    })
+  })
+
+  /**
      * We currently also have Sinon as a development dependency in the
      * instance where API calls are necessary. An example for this
      * will be added when this becomes necessary.
      */
-});
-
+})
