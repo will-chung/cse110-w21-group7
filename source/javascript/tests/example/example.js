4701206904abe179fea78b17ca5d9118e42b3537
Noah Teshima <noah.teshima@gmail.com>
2021-05-09 20:06:33 -0700

    Skeleton code for testing with Mocha and Chai

---
+++ source/javascript/tests/example/example.js
@@ -0,0 +1,48 @@
+class Foo {
+    constructor(str = '', num = 0, arr = []) {
+        this.str = str;
+        this.num = num;
+        this.arr = arr;
+    }
+
+    get string() {
+        return this.str;
+    }
+
+    set string(str) {
+        if (typeof(str) !== 'string') {
+            throw new TypeError();
+        }
+        this.str = str;
+    }
+
+    get number() {
+        return this.num;
+    }
+
+    set number(num) {
+        if (typeof(num) !== 'number') {
+            throw new TypeError();
+        }
+
+        this.number = num;
+    }
+
+    get array() {
+        return this.arr;
+    }
+
+    set array(arr) {
+        if (typeof(arr) !== 'object') {
+            throw new TypeError();
+        }
+
+        this.arrray = arr;
+    }
+}
+
+
+
+module.exports = {
+    Foo: Foo
+}
