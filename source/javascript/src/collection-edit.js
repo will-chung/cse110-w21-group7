48439b1696f33c1ad56df25b5b6cf5c9f01e303f
Yuzi Lyu <yul134@ucsd.edu>
2021-05-13 18:28:14 -0700

    style fix

--- source/javascript/src/collection-edit.js
+++ source/javascript/src/collection-edit.js
@@ -1,55 +1,55 @@
-var collapse = document.getElementById("collapse");
-var imageBox = document.getElementById("image-collection");
-var videoBox = document.getElementById("video-collection");
-var gallery = document.getElementById("media-gallery");
+const collapse = document.getElementById('collapse');
+const imageBox = document.getElementById('image-collection');
+const videoBox = document.getElementById('video-collection');
+const gallery = document.getElementById('media-gallery');
 /*
  * This onclick toggles the display style of the media gallery
  * TODO: When onclick, the size of the media gallery should be changed
  * 
  */
-collapse.addEventListener("click", () => {
-  if (collapse.innerHTML == "expand") {
-    collapse.innerHTML = "collapse";
-    imageBox.style.visibility = "visible";
-    videoBox.style.display = "block";
-    gallery.style.visibility = "visible";
+collapse.addEventListener('click', () => {
+  if (collapse.innerHTML == 'expand') {
+    collapse.innerHTML = 'collapse';
+    imageBox.style.visibility = 'visible';
+    videoBox.style.display = 'block';
+    gallery.style.visibility = 'visible';
   } else {
-    collapse.innerHTML = "expand";
-    imageBox.style.visibility = "hidden";
-    videoBox.style.display = "none";
-    gallery.style.visibility = "hidden";
+    collapse.innerHTML = 'expand';
+    imageBox.style.visibility = 'hidden';
+    videoBox.style.display = 'none';
+    gallery.style.visibility = 'hidden';
   }
 })
 function newElement() {
-  var span = document.createElement("select");
-  span.className = "dropdown";
-  var txt = document.createElement("option");
-  let close = document.createElement("option");
-  let complete = document.createElement("option");
-  close.text = "delete";
-  close.value = "close";
-  close.className = "close";
-  complete.text = "complete";
-  complete.value = "complete";
-  complete.className = "complete";
-  txt.text = "options";
-  txt.value = "value";
-  //span.className = "select";
+  var span = document.createElement('select');
+  span.className = 'dropdown';
+  var txt = document.createElement('option');
+  let close = document.createElement('option');
+  let complete = document.createElement('option');
+  close.text = 'delete';
+  close.value = 'close';
+  close.className = 'close';
+  complete.text = 'complete';
+  complete.value = 'complete';
+  complete.className = 'complete';
+  txt.text = 'options';
+  txt.value = 'value';
+  //span.className = 'select';
   span.appendChild(txt);
   span.appendChild(close);
   span.appendChild(complete);
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('myInput').value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(span);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert('You must write something!');
   } else {
     //span.appendChild(li);
-    //document.getElementById("myUL").appendChild(span);
-    document.getElementById("myUL").appendChild(li);
+    //document.getElementById('myUL').appendChild(span);
+    document.getElementById('myUL').appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById('myInput').value = '';
 
 }
--- source/javascript/src/daily.js
+++ source/javascript/src/daily.js
@@ -1,6 +1,6 @@
-var collapse = document.getElementById("collapse");
-var right = document.getElementById("right");
-var quote = document.getElementById("quote");
+var collapse = document.getElementById('collapse');
+var right = document.getElementById('right');
+var quote = document.getElementById('quote');
 console.log(right.style);
 /*
  * This onclick toggles the display style of the quote to none
@@ -8,47 +8,47 @@ console.log(right.style);
  * Resource: https://codepen.io/Mdade89/pen/JKkYGq
  * the link above provides a collapsible text box
  */
-collapse.addEventListener("click", ()=>{
-  if(quote.style.display == "none"){
-    collapse.innerHTML = "collapse";
-    right.style.visibility = "visible";
-    quote.style.display = "block";
+collapse.addEventListener('click', ()=>{
+  if(quote.style.display == 'none'){
+    collapse.innerHTML = 'collapse';
+    right.style.visibility = 'visible';
+    quote.style.display = 'block';
   }else{
-    collapse.innerHTML = "expand";
-    right.style.visibility = "hidden";
-    quote.style.display = "none";
+    collapse.innerHTML = 'expand';
+    right.style.visibility = 'hidden';
+    quote.style.display = 'none';
   }
 })
 function newElement() {
-    var span = document.createElement("select");
-    span.className="dropdown";
-    var txt = document.createElement("option");
-    let close = document.createElement("option");
-    let complete = document.createElement("option");
-    close.text = "delete";
-    close.value = "close";
-    close.className="close";
-    complete.text = "complete";
-    complete.value = "complete";
-    complete.className="complete";
-    txt.text = "options";
-    txt.value = "value";
-    //span.className = "select";
+    var span = document.createElement('select');
+    span.className='dropdown';
+    var txt = document.createElement('option');
+    let close = document.createElement('option');
+    let complete = document.createElement('option');
+    close.text = 'delete';
+    close.value = 'close';
+    close.className='close';
+    complete.text = 'complete';
+    complete.value = 'complete';
+    complete.className='complete';
+    txt.text = 'options';
+    txt.value = 'value';
+    //span.className = 'select';
     span.appendChild(txt);
     span.appendChild(close);
     span.appendChild(complete);
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('myInput').value;
     var t = document.createTextNode(inputValue);
     li.appendChild(span);
     li.appendChild(t);
     if (inputValue === '') {
-      alert("You must write something!");
+      alert('You must write something!');
     } else {
       //span.appendChild(li);
-        //document.getElementById("myUL").appendChild(span);
-      document.getElementById("myUL").appendChild(li);
+        //document.getElementById('myUL').appendChild(span);
+      document.getElementById('myUL').appendChild(li);
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById('myInput').value = '';
   
   }
