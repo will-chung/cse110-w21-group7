a622806efd8afca865f0765b6a05360f185c22d9
Yuzi Lyu <yul134@ucsd.edu>
2021-05-18 19:10:37 -0700

    linting fix

--- source/javascript/src/index.js
+++ source/javascript/src/index.js
@@ -1,10 +1,10 @@
 (function () {
   const $body = document.body
-	 const $menu_trigger = $body.getElementsByClassName('menu-trigger')[0]
+  const $menuTrigger = $body.getElementsByClassName('menu-trigger')[0]
 
-  if (typeof $menu_trigger !== 'undefined') {
-    $menu_trigger.addEventListener('click', function () {
-      $body.className = ($body.className == 'menu-active') ? '' : 'menu-active'
+  if (typeof $menuTrigger !== 'undefined') {
+    $menuTrigger.addEventListener('click', function () {
+      $body.className = ($body.className === 'menu-active') ? '' : 'menu-active'
     })
   }
 }).call(this)
