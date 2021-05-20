01cf27b72021872232506bcae3b14235ebd5bc11
Yuzi Lyu <yul134@ucsd.edu>
2021-05-20 12:27:30 -0700

    need review

--- source/javascript/src/weekly.js
+++ source/javascript/src/weekly.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
-});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar')
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  })
+  calendar.render()
+})
