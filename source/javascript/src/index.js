dd915b18c9d7dc144ed52ea3824e0d742f19e57f
Noah Teshima <noah.teshima@gmail.com>
2021-05-22 15:17:32 -0700

    Added read/write transactions for Add+ button

--- source/javascript/src/index.js
+++ source/javascript/src/index.js
@@ -1,215 +1,63 @@
 import {DateConverter as DateConverter} from './utils/DateConverter.js'
-
-let dailyLogs = [
-  {
-    "type": "object",
-    "required": [ "date", "description" ],
-    "properties": {
-      "date": {
-        "type": "string",
-        "time": "1621556127975",
-        "description": "The date of the event."
-      },
-      "events": [
-        {
-          "description": "description for event one",
-          "logType": "event",
-          "time": "1621427132543"
-        },
-        {
-          "description": "description for event two",
-          "logType": "event",
-          "time": "1621470343054"
-        }
-      ],
-      "tasks": [
-          {
-            "description": "description for task one",
-            "logType": "task",
-            "finished": true
-          },
-          {
-            "description": "description for task two",
-            "logType": "task",
-            "finished": false
-          }
-      ],
-      "notes": [
-        {
-          "description": "description for note one",
-          "logType": "note"
-        },
-        {
-          "description": "description for note two",
-          "logType": "note"
-        }
-    ],
-      "mood": {
-        "type": "number",
-        "multipleOf": 1,
-        "minimum": 0,
-        "exclusiveMaximum": 100,
-        "description": "Daily mood on a range of 0-99."
-      }
-    }
-  },
-  {
-    "type": "object",
-    "required": [ "date", "description" ],
-    "properties": {
-      "date": {
-        "type": "string",
-        "time": "16214434693434",
-        "description": "The date of the event."
-      },
-      "events": [
-        {
-          "description": "another description for event one",
-          "logType": "event",
-          "time": "1621427132543"
-        },
-        {
-          "description": "another description for event two",
-          "logType": "event",
-          "time": "1621470343054"
-        }
-      ],
-      "tasks": [
-          {
-            "description": "another description for task one",
-            "logType": "task",
-            "finished": true
-          },
-          {
-            "description": "another description for task two",
-            "logType": "task",
-            "finished": false
-          }
-      ],
-      "notes": [
-        {
-          "description": "another description for note one",
-          "logType": "note"
-        },
-        {
-          "description": "another description for note two",
-          "logType": "note"
-        }
-    ],
-      "mood": {
-        "type": "number",
-        "multipleOf": 1,
-        "minimum": 0,
-        "exclusiveMaximum": 100,
-        "description": "Daily mood on a range of 0-99."
-      }
-    }
-  }
-]
+import {IndexedDBWrapper as IndexedDBWrapper} from './indexedDB/IndexedDBWrapper.js'
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
   let addDailyLog = document.querySelector("button[id='rapid-log'] > a");
 
   addDailyLog.onclick = function(event) {
+    // Allows us to make write transactions before navigating
     event.preventDefault()
-    initIndexedDB("experimentalDB5", 1, () => {})
+    let wrapper = new IndexedDBWrapper("experimentalDB", 1);
+    wrapper.transaction((event) => {
+      let db = event.target.result
+
+      let transaction = db.transaction(['currentLogStore'], 'readwrite')
+      // Event triggered when transaction is successfully opened
+      transaction.oncomplete = (event) => {
+        console.log('Transaction completed.')
+      }
+
+      transaction.onerror = (event) => {
+        console.log('Transaction error.')
+      }
+
+      transaction.onsuccess = (event) => {
+        console.log('Transaction succeeded.')
+      }
+
+      let store = transaction.objectStore('currentLogStore')
+
+      let req = store.openCursor()
+      // Fires when cursor is successfully opened.
+      req.onsuccess = (event) => {
+        let cursor = event.target.result
+        if(cursor) {
+          console.log(cursor.value.current_log)
+          cursor.value.current_log = String(Date.now())
+          cursor.update(cursor.value)
+        }else {
+          // This block of execution occurs if we have no entries set up under
+          // object storage (i.e. new user who clicked on 'Add+')
+          let tempObjectStore = db.transaction(['currentLogStore'], 'readwrite')
+                                        .objectStore('currentLogStore')
+          const req = new XMLHttpRequest()
+          req.onreadystatechange = () => {
+              if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+                  const response = JSON.parse(req.responseText)
+                  let tempStore = db.transaction('currentLogStore', 'readwrite').objectStore('currentLogStore')
+                  response.current_log = String(Date.now())
+                  tempStore.put(response)
+              }
+          }
+          req.open('GET', '../models/schema.json')
+          req.send()
+          console.log('Made new entry!')
+        }
+        // Unsuspend navigation
+        window.location.href = 'daily.html'
+      }
+    })
   }
 })
-
-/**
- * Function to intiialize indexedDB and run a callback on
- * transactions with daily log storage object.
- * @param {String} name String contining the name of the database
- * indexedDB should open
- * @param {Number} version Version number containing the version 
- * that indexedDB should open
- */
-function initIndexedDB(name="experimentalDB", version=1, cb) {
-  let request = window.indexedDB.open(name, version)
-  // Event triggered if opening the DB doesn't work
-  request.onerror = function(event) {
-    console.log("IndexedDB not working")
-  }
-
-  request.onupgradeneeded = function(event) {
-    // Save the IDBDatabase interface
-    let db = event.target.result
-    // Create an objectStore for this database
-    if (!db.objectStoreNames.contains('dailyLogsStore')) {
-      let objectStore
-      // create object store in our DB
-      objectStore = db.createObjectStore("dailyLogsStore", { keyPath: "properties.date.time"})
-      objectStore.createIndex("event", "properties.events", { unique: false })
-
-      
-      
-      objectStore.transaction.oncomplete = (event) => {
-        let tempObjectStore = db.transaction(['dailyLogsStore'], 'readwrite')
-                                .objectStore('dailyLogsStore')
-        tempObjectStore.transaction.oncomplete = (event) => {
-          console.log('Added data to object store.')
-        }
-        // Storing mock daily logs data
-        dailyLogs.forEach((user) => {
-          tempObjectStore.add(user)
-          console.log(`Added ${user}`)
-        })
-      }
-      // Make sure objectStore has been completed using transaction.oncomplete
-    } else {
-      // open existing object store
-      console.log('Object store already created!')
-    }
-  };
-
-
-  // Event triggered if opening the DB works
-  request.onsuccess = function(event) {
-    console.log("IndexedDB working")
-    let db = request.result
-
-    let transaction = db.transaction(['dailyLogsStore'], 'readwrite')
-    // Event triggered when transaction is successfully opened
-    transaction.oncomplete = (event) => {
-      console.log('Transaction completed.')
-    }
-
-    transaction.onerror = (event) => {
-      console.log('Transaction error.')
-    }
-
-    transaction.onsuccess = (event) => {
-      console.log('Transaction succeeded.')
-    }
-
-    let store = transaction.objectStore('dailyLogsStore')
-
-    // let logRequest = store.get("1621445109502")
-    // logRequest.onsuccess = (event) =>{
-    // }
-    
-
-    store.openCursor().onsuccess = (event) => {
-      let cursor = event.target.result
-      let converter = new DateConverter()
-      const currentDay = converter.getDaysFromTimeStamp()
-      if (cursor) {
-        converter.timestamp = Number(cursor.value.properties.date.time)
-        const aux = converter.getDaysFromTimeStamp()
-        if(aux === currentDay) {
-          console.log('Success!')
-        }        
-        cursor.continue()
-      }
-    }
-
-    // let ind = store.index('entry')
-    // ind.get(1).onsuccess = (event) => {
-    //   console.log("Timestamp is: " + event.target.result.timestamp)
-    // }
-
-  }
-}
-
 
