435574acc3ce94738b5c92d8d7359c6816423bc8
Katherine Baker <klbaker@ucsd.edu>
2021-06-01 14:48:44 -0700

    DateConverter unit testing completed

--- source/javascript/tests/date-converter.test.js
+++ source/javascript/tests/date-converter.test.js
@@ -8,20 +8,43 @@
 import { DateConverter } from '../src/utils/DateConverter.js'
 const should = require('chai').should()
 
-const testConverter = new DateConverter()//Date.UTC('2021','03','24','22','35','30'))
+// tester is the UNIX timestamp for  May 24, 2021 at 22:35:30 (PDT)
+const testDate = new Date('May 24, 2021 22:35:30').getTime()
 
-// tester is the UNIX timestamp for  May 23, 2021 at 22:35:30
-const testDate = new Date(Date.UTC('2021','03','24','22','35','30'))
-const today = Date.now()
+describe ('Testing DateConverter', function() {
+    let actualNow
+    let testConverter
 
-it("should convert days since January 1, 1970 correctly", () => {
-    testConverter.getDaysFromTimeStamp(testDate).should.equal(18770)
+    beforeEach(function () {
+        actualNow = Date.now;
+        Date.now = fakeNow();
+        testConverter = new DateConverter(Date.now)
+     })
+  
+     afterEach(function () {
+        Date.now = actualNow;
+     })
+
+    it("should convert days since January 1, 1970 correctly", () => {
+        testConverter.getDaysFromTimeStamp(testDate).should.equal(18772)
+    })
+    
+    it("should determine that today is not May 24", () => {
+        testConverter.equals(testDate).should.equal(false);
+    })
+    
+    it("should determine today properly", () => {
+        testConverter.equals(Date.now).should.equal(true);
+    })
 })
 
-it("should determine that today is not May 24", () => {
-    testConverter.equal(testDate).should.be(false);
-})
 
-it("should determine today properly", () => {
-    testConverter.equal(today).should.be(true);
-})
+/**
+ * @author Katherine Baker <klbaker@ucsd.edu>
+ * Helper function in order to change the value of Date.now() so that the unit
+ * tests can be reused
+ */
+function fakeNow() {
+    // mock now = 	1621834530 sec = May 23, 2021 at 22:35:30 (PDT)
+    return 	1621834530000;
+ }
