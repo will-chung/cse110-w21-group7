4d7a47cedc81b7cbd14731206d55eb92cb339b50
Noah Teshima <noah.teshima@gmail.com>
2021-06-02 23:23:58 -0700

    Added routing and business/presentation logic for search components

--- source/javascript/src/components/SearchItem.js
+++ source/javascript/src/components/SearchItem.js
@@ -1,3 +1,6 @@
+import { DateConverter } from '/source/javascript/src/utils/DateConverter.js'
+import { Router, ROUTES } from '/source/javascript/src/utils/Router.js'
+
 /**
  * Component class used to list daily-logs on
  * the search page.
@@ -14,8 +17,14 @@ class SearchItem extends HTMLElement {
     this.attachShadow({ mode: 'open' })
     // Unfortunately this cannot be made a private field, since ESLint does not properly
     // lint private fields.
-    this._entry = {}
-    this._entry.description = ''
+    this._entry = {
+      time: undefined,
+      tasks: [],
+      notes: [],
+      events: [],
+      reflection: [],
+      type: 'daily-log'
+    }
     this.render()
   }
 
@@ -23,32 +32,104 @@ class SearchItem extends HTMLElement {
     this.shadowRoot.innerHTML = `<style>
                                       </style>
                                       <span id="single-entry">
-                                          <a href="#" class="result" target="_blank">${this._itemEntry.description}</a>
+                                          <a href="#" class="result" target="_blank">${this.getDate()}</a>
+                                          <ul id="log-entries">
+                                          </ul>
                                       </span>`
+    let that = this
+    // If we are displaying results for daily logs
+    if (this._entry.type === 'daily-log') {
+      // this.populateLogEntries()
+      this.shadowRoot.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault()
+        const params = new URLSearchParams()
+        params.append('timestamp', Number(that._entry.time))
+        const url = new URL(ROUTES.daily, window.location.origin)
+        url.search = params
+        new Router(url).navigate()
+      })
+      this.populateLogEntries()
+    } else {
+      this.shadowRoot.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault()
+        const params = new URLSearchParams()
+        params.append('name', encodeURIComponent(that._entry.name))
+        const url = new URL(ROUTES['collection-edit'], window.location.origin)
+        url.search = params
+        new Router(url).navigate()
+      })
+      this.populateCollectionTasks()
+    }
   }
 
   /**
-       * Setter for private field itemEntry, containing
-       * the logType, description, and date of our search
-       * item.
+   * Subroutine used in order to show the relevant tasks, notes,
+   * events, and reflection data in the contained daily log as
+   * list items.
+   */
+  populateLogEntries() {
+    this.addAsListItems(this._entry.tasks)
+    this.addAsListItems(this._entry.notes)
+    this.addAsListItems(this._entry.events)
+    this.addAsListItems(this._entry.reflection)
+  }
+
+  /**
+   * Subroutine used in order to show the relevant tasks
+   * in the contained collection as list items.
+   */
+  populateCollectionTasks() {
+    this.addAsListItems(this._entry.tasks)
+  }
+
+  /**
+   * Business/presentation logic subroutine used in order
+   * to display entries as list items on the current search component.
+   * @param {Object[]} list Array of JSON objects with which to create
+   * list entries
+   */
+  addAsListItems(list) {
+    let that = this
+    list.forEach((item) => {
+      let li = document.createElement('li')
+      li.textContent = item['item'].description
+      that.shadowRoot.getElementById('log-entries').appendChild(li)
+    })
+  }
+
+  /**
+   * Subroutine used in order to show the date of the
+   * entry being shown
+   */
+  getDate() {
+    let timestamp = this._entry.time
+    const date = new DateConverter(timestamp)
+    return date.toLocaleDateString()
+  }
+
+
+
+  /**
+       * Setter for private field entry, containing
+       * the tasks, notes, events, reflections, description,
+       * and date of our search item.
        * @param {Object} entry JSON object containing the
        * new fields for our search item.
        */
-  set itemEntry (entry) {
-    entry.editable = true
-    this._itemEntry = entry
+  set entry (entry) {
+    this._entry = entry
     this.render()
   }
 
   /**
-       * Getter for private field itemEntry, containing
-       * the logType, description, and date of our search
-       * item.
+       * Getter for private field entry, containing
+       * the tasks, notes, events, reflections, description,
+       * and date of our search item.
        * @return {Object} JSON object containing the
        * new fields for our search item.
        */
-  get itemEntry () {
-    return this._itemEntry
+  get entry () {
+    return this._entry
   }
 }
 
