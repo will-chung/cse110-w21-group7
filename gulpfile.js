e9b076b67a73a684a61eb9b3db5a0ac72986db7d
Noah Teshima <noah.teshima@gmail.com>
2021-06-08 01:07:06 -0700

    Added minification for CSS, HTML; transpiling to ES5

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,76 @@
-const { src, dest } = require('gulp')
+const gulp = require('gulp')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
 
-function defaultTask(cb) {
+const SRC = {
+    html: 'source',
+    css: 'source/css',
+    js: 'source/javascript/src'
+}
+
+/**
+ * Gulp task used to transpile from ES6 to ES5.
+ * This is necessary for Gulp to properly minify
+ * files.
+ * @param {Function} cb Gulp callback function. Used
+ * to indicate when our task finishes
+ */
+function transpile(cb) {
+    gulp.src(`${SRC.js}/**/*.js`)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest(`${SRC.js}/`))
     cb();
 }
 
+/**
+ * Gulp task used to minify and clean CSS for production.
+ * @param {Function} cb Gulp callback function. Used
+ * to indicate when our task finishes
+ */
 function minifyCSS(cb) {
+    gulp.src(`${SRC.css}/**/*.css`)
+        .pipe(cleanCSS('source/target'))
+        .pipe(gulp.dest(`${SRC.css}/`))
     cb();
 }
 
+/**
+ * Gulp task used to minify and clean HTML for production.
+ * @param {Function} cb Gulp callback function. Used
+ * to indicate when our task finishes
+ */
 function minifyHTML(cb) {
+    gulp.src(`${SRC.html}/*.html`)
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+         }))
+        .pipe(gulp.dest(`${SRC.html}/`))
     cb();
 }
 
+/**
+ * Gulp task used to minify and clean JavaScript for production.
+ * @param {Function} cb Gulp callback function. Used
+ * to indicate when our task finishes
+ */
 function minifyJS(cb) {
-    src('source/javascript/src/**/**.js')
-    .pipe(uglify({ 
-        preserveComments: false
-    }))
-    .pipe(rename(function(path) {
-        return {
-            dirname: path.dirname,
-            basename: path.basename,
-            extname: path.extname
-        }
-    }))
-    .pipe(dest(function(file) {
-        return file.dest
-    }))
+    gulp.src('${SRC.js}/**/*.js')
+        .pipe(uglify().on('error', console.error))
+        .pipe(rename(function(path) {
+            return {
+                dirname: path.dirname,
+                basename: path.basename,
+                extname: path.extname
+            }
+        }))
+        .pipe(gulp.dest('${SRC.js}/'));
     cb();
 }
 
-exports.minify = parallel(minifyHTML, minifyCSS, minifyJS)
-exports.diff = diffTask
-
+exports.default = gulp.series(transpile, gulp.parallel(minifyHTML, minifyCSS, minifyJS))
